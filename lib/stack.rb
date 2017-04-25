2a3a2112aad0a67401235fbbc12490601e25edf9
Jovanny Cruz <jovannypcg@yahoo.com>
2017-04-25 16:45:43 -0500

    Add stack implementation

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,63 @@
+require_relative './node'
+
 class Stack
-  
+  def initialize
+    @head = nil
+  end
+
+  # Adds a new item to the stack.
+  # A Node object is created and then inserted into the stack.
+  def push(data)
+    new_node = Node.new(data)
+
+    if empty?
+      @head = new_node
+    else
+      new_node.next = @head
+      @head = new_node
+    end
+  end
+
+  # Returns the stack's size.
+  def size
+    return 0 if empty?
+
+    count = 0
+    current_node = @head
+
+    while current_node
+      count += 1
+      current_node = current_node.next
+    end
+
+    count
+  end
+
+  # Verifies the stack is empty.
+  # Returns true if the stack is empty, false otherwise.
+  def empty?
+    @head.nil?
+  end
+
+  # Clears the stack by setting @head to nil.
+  def clear
+    @head = nil
+  end
+
+  # Retrieves last item inserted in the stack
+  # without removing it.
+  def poll
+    return @head.data unless empty?
+  end
+
+  # Retrieves last item inserted in the stack
+  # and removes it.
+  def pop
+    return nil if empty?
+
+    data = @head.data
+    @head = @head.next
+
+    data
+  end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -33,38 +33,40 @@ describe Stack do
 
       # Should not this example raises an error?
       it "returns 'nil' when stack is empty" do
-        value = stack.pop
+        stack.clear
 
+        value = stack.pop
         expect(value).to be_nil
       end
     end
 
     describe '#poll' do
       it 'retrieves item without removing it' do
-        expect(stack.size).to eql(4)
+        expect(stack.size).to eql(10)
 
         value = stack.poll
 
-        expect(value).to eql(8)
-        expect(stack.size).to eql(4)
+        expect(value).to eql(10)
+        expect(stack.size).to eql(10)
       end
 
       # Should not this example raises an error?
       it "returns 'nil' when stack is empty" do
+        stack.clear
         expect(stack.poll).to be_nil
       end
     end
   end
 
-  describe '#.clear' do
+  describe '#clear' do
     it 'flushes the stack' do
       stack.clear
       expect(stack.size).to eql(0)
     end
   end
 
-  describe '#.size' do
-    it 'returns 0 when recently instantiated' do
+  describe '#size' do
+    it 'returns 0 when no items' do
       expect(stack.size).to eql(0)
     end
 
@@ -72,6 +74,24 @@ describe Stack do
       3.times { stack.push(Object.new) }
 
       expect(stack.size).to eql(3)
+    end
+  end
+
+  describe '#empty?' do
+    context 'stack contains items' do
+      it "returns 'false'" do
+        3.times { |num| stack.push(num) }
+
+        expect(stack.empty?).to be_falsey
+      end
+    end
+
+    context 'stack does not containt items' do
+      it "returns 'true'" do
+        stack.clear
+
+        expect(stack.empty?).to be_truthy
+      end
     end
   end
 end
